ac5bb1800d109e402e066305da812af6ab621dbc
James Dabbs <jamesdabbs@gmail.com>
2014-12-23 11:43:59 -0500

    Update Proc#arguments extension
    The existing implementation depended on ParseTree, which does not
    support Ruby 2.0+. Fortunately, Ruby 1.9.2+ includes the `parameters`
    method which does exactly what we need.

--- lib/scruby/core_ext/proc.rb
+++ lib/scruby/core_ext/proc.rb
@@ -1,11 +1,6 @@
 class Proc
-
   def arguments
-    case self.arity
-    when -1..0 then []
-    when 1 then self.to_sexp.assoc( :dasgn_curr )[1].to_array
-    else self.to_sexp[2][1][1..-1].collect{ |arg| arg[1]  }
-    end
+    self.parameters.map &:last
   end
   alias :value :call
 end
