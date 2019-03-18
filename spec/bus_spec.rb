34f7278b7d4f92563b6975c75da00d72cc9c59c7
Javier Sánchez <javier.sy@gmail.com>
2019-03-18 16:15:58 +0100

    wip buses

--- spec/bus_spec.rb
+++ spec/bus_spec.rb
@@ -8,37 +8,88 @@ require 'scruby/server'
 
 include Scruby
 
+
 describe Bus do
-  before :all do
-    @server = Server.new
-    wait
-  end
-
-  after :all do
-    @server.quit
-    wait
-  end
-
-  describe 'instance' do
-    before :all do
-      @audio    = AudioBus.allocate @server, channels: 4
-      @control  = ControlBus.allocate @server, channels: 4
+  describe 'instantiation' do
+    before do
+      @server  = Server.new
+      @audio   = AudioBus.allocate @server
+      @control = ControlBus.allocate @server
     end
 
-    describe AudioBus do
+    it 'should be a bus' do
+      @audio.should be_a(Bus)
+      @control.should be_a(Bus)
+    end
+
+    it 'should not instantiate with new' do
+      lambda { Bus.new @server, :control, 1 }.should raise_error(NoMethodError)
+    end
+
+    it 'should set server' do
+      @audio.server.should == @server
+    end
+
+    it 'should set audio rate' do
+      @audio.rate.should == :audio
+    end
+
+    it 'should set control rate' do
+      Bus.control(@server).rate.should == :control
+    end
+
+    it 'should allocate in server on instantiation and have index' do
+      @server.audio_buses.should include(@audio)
+      @server.control_buses.should include(@control)
+    end
+
+    it 'should have index' do
+      @audio.index.should == 16
+      @control.index.should == 0
+    end
+
+    it 'should free and null index' do
+      @audio.free
+      @server.audio_buses.should_not include(@audio)
+      @audio.index.should == nil
+      @control.free
+      @server.audio_buses.should_not include(@control)
+      @control.index.should == nil
+    end
+
+    it 'should return as map if control' do
+      @control.to_map.should == 'c0'
+    end
+
+    it 'should raise error if calling to_map on an audio bus' do
+      lambda { @audio.to_map }.should raise_error(SCError)
+    end
+
+    it 'should print usefull information with to_s'
+
+    it 'should be hardware out' do
+      @server.audio_buses[0].should be_audio_out
+      @audio.should_not be_audio_out
+    end
+
+    describe 'multichannel' do
+      before do
+        @server   = Server.new
+        @audio    = AudioBus.allocate @server, channels: 4
+        @control  = ControlBus.allocate @server, channels: 4
+      end
+
       it 'should allocate consecutive when passing more than one channel for audio' do
-        @audio.rate.should == :audio
         @audio.index.should == 16
         buses = @server.audio_buses
-        buses[16..-1].count.should eq 4
-        AudioBus.allocate(@server).index.should == 20
+        buses[16..-1].should have(4).elements
+        Bus.audio(@server).index.should == 20
       end
 
       it 'should allocate consecutive when passing more than one channel for control' do
-        @control.rate.should == :control
         @control.index.should == 0
-        @server.control_buses.count.should eq 4
-        ControlBus.allocate(@server).index.should == 4
+        @server.control_buses.should have(4).elements
+        Bus.control(@server).index.should == 4
       end
 
       it 'should set the number of channels' do
@@ -56,60 +107,72 @@ describe Bus do
   end
 
   describe 'messaging' do
-    shared_examples_for 'bus' do
-      it { @bus.server.should == @server }
-      it { @bus.rate.should   == @rate }
-      it { @bus.index.should  == 0 }
-      it { @server.buses(@rate).should include @bus }
-
-      describe 'freeing' do
-        before { @bus.free }
-        it { @server.buses(@rate).should_not include @audio }
-        it { @bus.index.should == nil }
-      end
-    end
-
     before :all do
+      @server = Server.new
       @server.boot
       @server.send '/dumpOSC', 3
+      @bus = ControlBus.allocate @server, channels: 4
       wait
     end
 
-    describe ControlBus do
-      before do
-        @bus = ControlBus.allocate @server
-        @rate = :control
+    after :all do
+      @server.quit
+    end
+
+    describe 'set' do
+      it 'should send set message with one value' do
+        @bus.set 101
+        wait
+        unwind(@server.log).should =~ %r{\[ "/c_set", #{ @bus.index }, 101 \]}
       end
 
-      it { @bus.to_map.should == 'c0' }
-      it_should_behave_like 'bus'
-    end
-  end
-
-  describe 'class' do
-    shared_examples_for 'allocates multichannel buses' do
-      before do
-        @server.stub!(:allocate) { |type, buses| @server.send(type).push *buses }
-        @bus = subject.allocate_buses @server, 4
+      it 'should accept value list and send set with them' do
+        @bus.set 101, 202
+        wait
+        unwind(@server.log).should =~ %r{\[ "/c_set", #{ @bus.index }, 101, #{ @bus.index + 1}, 202 \]}
       end
 
-      it { @bus.should be_a Bus }
-      it { @bus.index.should == 0 }
-      it { @server.buses(subject::RATE).should have(4).buses }
-      it { @bus.channels.should == 4 }
-      it { @server.buses(subject::RATE).each { |bus| bus.main_bus.should == @bus } }
+      it 'should accept an array and send set with them' do
+        @bus.set [101, 202]
+        wait
+        unwind(@server.log).should =~ %r{\[ "/c_set", #{ @bus.index }, 101, #{ @bus.index + 1}, 202 \]}
+      end
+
+      it 'should warn but not set if trying to set more values than channels' do
+        @bus.should_receive(:warn).with("You tried to set 5 values for bus #{ @bus.index } that only has 4 channels, extra values are ignored.")
+        @bus.set 101, 202, 303, 404, 505
+        wait
+        unwind(@server.log).should =~ %r{\[ "/c_set", #{ @bus.index }, 101, #{ @bus.index + 1}, 202, #{ @bus.index + 2}, 303, #{ @bus.index + 3}, 404 \]}
+      end
     end
 
-    describe AudioBus do
-      subject { AudioBus }
-      it { subject::RATE.should == :audio }
-      it_should_behave_like 'allocates multichannel buses'
+    describe 'set' do
+      it 'should send fill just one channel' do
+        @bus.fill 101, 1
+        wait
+        unwind(@server.log).should =~ %r{\[ "/c_fill", #{ @bus.index }, 1, 101 \]}
+      end
+
+      it 'should fill all channels' do
+        @bus.fill 101
+        wait
+        unwind(@server.log).should =~ %r{\[ "/c_fill", #{ @bus.index }, 4, 101 \]}
+      end
+
+      it 'should raise error if trying to fill more than assigned channels' do
+        @bus.should_receive(:warn).with("You tried to set 5 values for bus #{ @bus.index } that only has 4 channels, extra values are ignored.")
+        @bus.fill 101, 5
+        wait
+        unwind(@server.log).should =~ %r{\[ "/c_fill", #{ @bus.index }, 4, 101 \]}
+      end
     end
 
-    describe ControlBus do
-      subject { ControlBus }
-      it { subject::RATE.should == :control }
-      it_should_behave_like 'allocates multichannel buses'
+    describe 'get' do
+      it 'should send get message with one value'
+      it 'should send get message for various channels'
+      it 'should accept an array and send set with them'
+      it 'should raise error if trying to set more values than channels'
+      it 'should actually get the response from the server'
     end
   end
 end
